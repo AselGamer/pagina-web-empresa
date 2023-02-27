08a3bbf7d5f75f33c7349c90389a14dc6d385879
AselGamer <35375698+AselGamer@users.noreply.github.com>
2023-02-27 10:22:13 +0100

    errores varios solucionados

--- editar.php
+++ editar.php
@@ -1,8 +1,40 @@
 <?php
 include 'bbdd.php';
 
+$ubicacionImgs = 'images/';
+
+$ubicacionDeseada = 'images/' . basename($_FILES['imagen']['name']);
+
+$temp = 'images/temp.png';
+
+
+if ($_FILES['imagen']['size'] > 1000000) {
+    header('Location: crearProducto.php?error=1');
+}
+
+// DO NOT TRUST $_FILES['upfile']['mime'] VALUE !!
+// Check MIME Type by yourself.
+$finfo = new finfo(FILEINFO_MIME_TYPE);
+if (false === $ext = array_search(
+    $finfo->file($_FILES['imagen']['tmp_name']),
+    array(
+        'jpg' => 'image/jpeg',
+        'png' => 'image/png',
+        'gif' => 'image/gif',
+    ),
+    true
+)) {
+    header('Location: crearProducto.php?error=2');
+}
+
+if (!empty($_FILES['imagen']['name'])) {
+    move_uploaded_file($_FILES['imagen']['tmp_name'], $ubicacionDeseada);
+} else {
+    header('Location: crearProducto.php?error=3');
+}
+
 if(isset($_POST['editar'])) {
-    $resultado = editarComponentes($_POST['id'], $_POST['descripcion'], $_POST['precio'], $_POST['nombre'], $_POST['tipo_componente'], $_POST['imagen'] );
+    $resultado = editarComponentes($_POST['id'], $_POST['descripcion'], $_POST['precio'], $_POST['nombre'], $_POST['tipo_componente'], $ubicacionDeseada );
     header('location: index.php');
 }
 
